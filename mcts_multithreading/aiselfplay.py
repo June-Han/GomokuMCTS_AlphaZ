296e7cb97855c47132358b406ef85563eb52c6bc
June-Han <julialiu08@gmail.com>
2025-04-15 02:28:49 +0800

    ai_vs_ai

--- mcts_multithreading/aiselfplay.py
+++ mcts_multithreading/aiselfplay.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 from board import PLAYER_A, PLAYER_B, TIE
 from gomoku_board import GomokuBoard
@@ -28,24 +29,30 @@ class ai_battle():
         self.ai1 = ai1
         self.ai2 = ai2
         self.axes = None
-        ai1_wins = 0
-        ai2_wins = 0
+        self.ai1_wins = 0
+        self.ai2_wins = 0
         self.game_results = None
 
         for i in range (0, self.total_games):
             self._selfplay()
-            if self.game_results == PLAYER_A:
+            if self.game_results == 1:
                 self.ai1_wins += 1
-            elif self.game_results == PLAYER_B:
+                print("Winner: ", self.result_text[PLAYER_A])
+            elif self.game_results == -1:
                 self.ai2_wins += 1
+                print("Winner: ", self.result_text[PLAYER_B])
             self._print_details(i)
             self.game_board.reset_board()
+            self.ai1.game_board.reset_board()
+            self.ai1.reset_ai(GomokuBoard)
+            self.ai2.game_board.reset_board()
+            self.ai2.reset_ai(GomokuBoard)
+            self.axes = None
         
-        f = open("results.txt", "a")
-        f.write("AI Player 1 (Black Stone) wins:", self.ai1_wins)
-        f.write("AI Player 2 (White Stone) wins:", self.ai2_wins)
-        f.write("AI Player 2 (White Stone) avg time:", self.ai2_wins)
-        f.close()
+        with open(os.path.join(os.getcwd(), "results.txt"), "a") as f:
+            print(f"{os.getcwd()}{os.sep}results.txt")
+            f.write("\nAI Player 1 (Black Stone) wins: " + str(self.ai1_wins))
+            f.write("\nAI Player 2 (White Stone) wins: " + str(self.ai2_wins))
         
 
     def _selfplay(self):
@@ -55,7 +62,7 @@ class ai_battle():
             self.ai1.update_state(ai1_best_next_move)
             self.ai2.update_state(ai1_best_next_move)
             self.game_board.update_state(ai1_best_next_move)
-            self.axes = self.game_board.draw(self.axes, 1)
+            self.axes = self.game_board.draw(self.axes, 3)
 
             #AI2
             if self.game_board.judge() is None:
@@ -70,19 +77,19 @@ class ai_battle():
         self.avg_time_ai1 = self.ai1.movetime/ai1.movecount
         self.avg_time_ai2 = self.ai2.movetime/ai2.movecount
         data = [
-            ["AI1 (Black Stone)", self.ai1.movecount, self.ai1.movetime, self.avg_time_ai1, self.result_text[self.game_results], self.ai1.min_num_sum],
-            ["AI2 (White Stone)", self.ai2.movecount, self.ai2.movetime, self.avg_time_ai2, self.result_text[self.game_results],self.ai2.min_num_sum]
+            [game_no+1, "AI1 (Black Stone)", self.ai1.movecount, self.ai1.movetime, self.avg_time_ai1, self.result_text[self.game_results], self.ai1.min_num_sim],
+            [game_no+1, "AI2 (White Stone)", self.ai2.movecount, self.ai2.movetime, self.avg_time_ai2, self.result_text[self.game_results],self.ai2.min_num_sim]
         ]
         table = tabulate(
             data, 
-            headers=["AI_Player","Total No of Moves", "Total Time Taken", "Avg Time Per Move(s)", "Winner", "Total Simulations"], 
+            headers=["Game No","AI_Player","Total No of Moves", "Total Time Taken", "Avg Time Per Move(s)", "Winner", "Total Simulations"], 
             tablefmt="grid"
         )
 
-        f = open("results.txt", "a")
-        f.write(game_no + 1)
-        f.write(table)
-        f.close()
+        with open(os.path.join(os.getcwd(), "results.txt"), "a") as f:
+            f.write("\n")
+            f.write(table)
+            f.write("\n")
 
         print("Game ", game_no+1)
         print(table)
@@ -93,4 +100,4 @@ if __name__ == '__main__':
     gboard = GomokuBoard()
     ai1 = MCUCT(GomokuBoard, min_num_sim=2e4)
     ai2 = MCUCT(GomokuBoard, min_num_sim=3e4)
-    app = ai_battle(gboard, ai1, ai2, 1)
+    app = ai_battle(gboard, ai1, ai2, 4)
